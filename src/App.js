db97039047baeea44675c9ccec69d124669a356d
ryanmonro <ryan@ryanmonro.info>
2020-04-21 21:25:17 +1000

    Changed header title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Song Generator
+        Hit Machine
       </header>
       <Song />
     </div>
